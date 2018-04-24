60d7c8415eb7f5f2810a91282bdb964fc1d11534
Unknown <fermartinotti@gmail.com>
2018-04-24 19:28:05 -0300

    Busqueda de reclamo funcionando en index con validacion solo front

--- frontend/src/app/inicio/inicio.component.ts
+++ frontend/src/app/inicio/inicio.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
+
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  numeroDeReclamo: number= 1;
+
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
+  buscarReclamo(id:number):void {
+  // validar primero o lanzar error
+    this.router.navigate(['mis-reclamos', id])
+  }
 }
