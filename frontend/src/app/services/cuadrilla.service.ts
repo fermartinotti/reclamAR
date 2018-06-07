9f9ad32ad4eefd328b53e949e192ca2b5ff375e5
Unknown <fermartinotti@gmail.com>
2018-06-06 21:03:38 -0300

    Servicios para consumir la apirest

--- frontend/src/app/services/cuadrilla.service.ts
+++ frontend/src/app/services/cuadrilla.service.ts
@@ -3,14 +3,22 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import 'rxjs/add/operator/map';
 import {Cuadrilla} from "../model/cuadrilla";
 import 'rxjs/add/operator/map';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class CuadrillaService {
   constructor(private httpClient: HttpClient) {
-  //
-  // public crearCuadrilla(cuadrilla: Cuadrilla): Promise<void>{
-  //     return await this.httpClient.post('http://localhost:8080/api/rest/')
-  //   }
-
   }
+
+   async crearCuadrilla(cuadrilla: Cuadrilla): Promise<any>{
+      return await this.httpClient.post('http://localhost:8080/api/rest/cuadrillas', cuadrilla,
+        {headers: new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`)
+    }).toPromise()
+  }
+
+  async todasLasCuadrillas(): Promise<Array<Cuadrilla>>{
+    return await this.httpClient.get<Array<Cuadrilla>>('http://localhost:8080/api/rest/cuadrillas').toPromise()
+  }
+
+
 }
