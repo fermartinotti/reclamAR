e82f7ecde4baf7d24390c460c387f6e7d906b693
Diego Garcia <garcia.diego1990@gmail.com>
2018-06-20 15:29:23 -0300

    refactor fechas "en reparacion" y "demorado"

--- backend/src/main/java/ar/edu/unq/reclamar/modelo/EnReparacion.java
+++ backend/src/main/java/ar/edu/unq/reclamar/modelo/EnReparacion.java
@@ -1,5 +1,7 @@
 package ar.edu.unq.reclamar.modelo;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
@@ -12,21 +14,19 @@ public class EnReparacion extends Estado {
 	private static final long serialVersionUID = 1L;
 	
 	@Column
-	public String fechaDeReparacion;
+	public LocalDate fechaDeReparacion;
+	
+	public LocalDate getFechaDeReparacion() {
+		return fechaDeReparacion;
+	}
+	public void setFechaDeReparacion(LocalDate fechaDeReparacion) {
+		this.fechaDeReparacion = fechaDeReparacion;
+	}
 
 	public EnReparacion() {}
 	
-	public EnReparacion(String fechaReparacion) {
-		this.fechaDeReparacion =fechaReparacion;
-	}
-
-	public String getFechaDeReparacion() {
-		return fechaDeReparacion;
-	}
-
-	public void setFechaDeReparacion(String fechaDeReparacion) {
+	public EnReparacion(LocalDate fechaDeReparacion) {
+		super();
 		this.fechaDeReparacion = fechaDeReparacion;
-	}
-	
-	
+	}	
 }
